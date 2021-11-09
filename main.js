fa816a64d511c144a2bc7513a203b162ac2a4751
Subho8888 <87801594+Subho8888@users.noreply.github.com>
2021-11-09 13:04:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+prediction1="";
+prediction2="";
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+
+
+});
+  camera=document.getElementById("camera");
+  Webcam.attach('#camera');
+
+function take_snapshot(){
+Webcam.snap(function(data_uri){
+document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'">'
+});
+  }
+
+  console.log('ml5version:',ml5.version);
+
+  classifer=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/FNa_XtZue/model.json',modelLoaded);
+
+  function modelLoaded(){
+      console.log('modelLoaded!');
+  }
+
+  function speak(){
+      var synth=window.speechSynthesis;
+      speak_data_1="The first Prediction is"+prediction1;
+      speak_data_2="And The Second Prediction is"+prediction2;
+      var utterThis=new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
+      synth.speak(utterThis);
+  }
